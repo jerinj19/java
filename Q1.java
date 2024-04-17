8f7707702ada10939e2122f65ab12c84439983c7
JERIN J <144670486+jerinj19@users.noreply.github.com>
2024-04-17 11:39:41 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,28 @@
+import package_graphics.*;
+import java.util.Scanner;
+public class Q1
+{
+public static void main(String []args)
+{
+package_graphics testObj = new package_graphics();
+int l,h,r,a,c,d;
+Scanner s=new Scanner(System.in);
+System.out.println("JERIN JOSE\n ROLLNO:35");
+System.out.println("Enter the length for rectangle");
+l=s.nextInt();
+System.out.println("Enter the breadth for rectangle");
+h=s.nextInt();
+System.out.println("Enter the radius of circle");
+r=s.nextInt();
+System.out.println("Enter the side for Square");
+a=s.nextInt();
+System.out.println("Enter the breadth for triangle");
+c=s.nextInt();
+System.out.println("Enter the height for triangle");
+d=s.nextInt();
+System.out.println("Area of rectangle="+testObj.recArea(l,h));
+System.out.println("Area of circle="+testObj.cirArea(r));
+System.out.println("Area of square="+testObj.squArea(a));
+System.out.println("Area of triangle="+testObj.triArea(c,d));
+}
+}
